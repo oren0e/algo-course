9adc00098abdf71ccb191bf7760df1c5a52f8632
oren0e <countx@gmail.com>
2020-07-02 14:07:19 +0300

    added karatsuba attempt.

--- course1_divide_and_conquer/ex1.py
+++ course1_divide_and_conquer/ex1.py
@@ -28,13 +28,37 @@ def recursive_mult(x: int, y: int) -> int:
     num_digits = int(math.log10(x) + 1)
     return 10**num_digits*ac + 10**(num_digits // 2)*(ad+bc) + bd
 
+def karatsuba(x: int, y: int) -> int:
+    # base case
+    if ((x // 10) == 0) or ((y // 10) == 0):
+        small: int = x * y
+    else:
+        small: int = 1
+
+    a, b = split_num(x)
+    c, d = split_num(y)
+    ac = karatsuba(a, c)*small
+    bd = karatsuba(b, d)
+    abcd = karatsuba(a+b, c+d)
+
+    gaus_trick = abcd - ac - bd 
+
+
+    # we are assuming that the two numbers have equal number of digits
+    num_digits = int(math.log10(x) + 1)
+    return 10**num_digits*ac + 10**(num_digits // 2)*(gaus_trick) + bd
+
+karatsuba(2345679, 234)
 
 # test cases
 assert recursive_mult(1234, 5678) == 1234*5678
 assert recursive_mult(10, 2) == 10*2
 assert recursive_mult(1, 1) == 1*1
 assert recursive_mult(2345679, 234) == 2345679*234, "We assumed that the numbers have the same digit length"
+assert recursive_mult(1111111111111111,22222222222222) == 1111111111111111*22222222222222
 
-# answer to exercise
-assert recursive_mult(3141592653589793238462643383279502884197169399375105820974944592, 2718281828459045235360287471352662497757247093699959574966967627) == \
-3141592653589793238462643383279502884197169399375105820974944592 * 2718281828459045235360287471352662497757247093699959574966967627
+assert karatsuba(1234, 5678) == 1234*5678
+assert karatsuba(10, 2) == 10*2
+assert karatsuba(1, 1) == 1*1
+assert karatsuba(2345679, 234) == 2345679*234, "We assumed that the numbers have the same digit length"
+assert karatsuba(1111111111111111,22222222222222) == 1111111111111111*22222222222222
