ea184a65ed33f622a04e0c4521cb725de3a3fa42
Oren Epshtain <oren@gojuno.com>
2020-08-16 02:33:42 +0300

    changed the printing of adjacency list and added checking for existing nodes in add_edge method. Ready to start thinking about the contraction problem.

--- course1_divide_and_conquer/ex4.py
+++ course1_divide_and_conquer/ex4.py
@@ -23,8 +23,25 @@ class Graph:
         new_node = Node(new_node_val)
         self.nodes.append(new_node)
 
-    def add_edge(self, node1: Node, node2: Node) -> None:
-        new_edge = Edge(node1, node2)
+    def add_edge(self, node1_value: int, node2_value: int) -> None:
+        node1_found = None
+        node2_found = None
+        # check if nodes already exist
+        for node in self.nodes:
+            if node1_value == node.value:
+                node1_found = node
+            if node2_value == node.value:
+                node2_found = node
+        if node1_found is None:
+            node1_found = Node(node1_value)
+            self.nodes.append(node1_found)
+        if node2_found is None:
+            node2_found = Node(node2_value)
+            self.nodes.append(node2_found)
+
+        new_edge = Edge(node1_found, node2_found)
+        node1_found.edges.append(new_edge)
+        node2_found.edges.append(new_edge)
         self.edges.append(new_edge)
 
     def get_adjacency_list(self) -> List[List[int]]:
@@ -32,14 +49,17 @@ class Graph:
         Return list of lists. Outer list index is the Node value.
         Inner list has the adjacent nodes values.
         '''
-        lst = [None for _ in range(len(self.nodes))]
-        for i, node in enumerate(self.nodes, 1):
-            if node.value == i:
-                inner_list = []
-                for edge in node.edges:
-                    inner_list.append(edge.node2.value)
+        lst = [None for _ in range(len(self.nodes) + 1)]
+        for node in self.nodes:
+            if (len(node.edges) == 1) and (node.edges[0].node2.value == node.value):
+                continue
+            inner_list = []
+            for edge in node.edges:
+                if edge.node2.value == node.value:
+                    continue
+                inner_list.append(edge.node2.value)
             if inner_list:
-                lst[i] = inner_list
+                lst[node.value] = inner_list
         return lst
 
 # read the input
@@ -55,3 +75,11 @@ with open('kargerMinCut.txt', 'r') as f:
 for i, sub_list in enumerate(input_list):
     for j, num in enumerate(sub_list):
         input_list[i][j] = int(input_list[i][j])
+
+# read into Graph class
+grph = Graph()
+for sub_list in input_list:
+    for num in sub_list[1:]:
+        grph.add_edge(sub_list[0], num)
+
+res = grph.get_adjacency_list()     # res[0] is None. res[1] to res[200] contain the data.
