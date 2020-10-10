5b05285bf4047821098fcbced994f19c0702510b
Oren Epshtain <oren@gojuno.com>
2020-10-10 18:43:28 +0300

    wrote function to switch indices as well as node values for the 2nd pass.

--- course2_graph_serach/ex1.py
+++ course2_graph_serach/ex1.py
@@ -30,7 +30,8 @@ class Node:
         self.seen = False
         self.seen2 = False
         self.leader: Optional[Node] = None
-        #self.to_nodes: List[Node] = []
+        self.old_value = None
+        self.been_switched = False
 
     def __lt__(self, other: Node) -> bool:
         return self.finish_time < other.finish_time
@@ -83,9 +84,9 @@ def find_max_node_value(file: str) -> int:
 #   does not appear, it will be None. This None can interfere, check that out!
 
 
-def read_input_to_graph(file: str, reversed: bool = False) -> Tuple[Graph, Graph]:
-    output_list: Graph = [None for _ in range(find_max_node_value(file))]
-    to_nodes: Union[Graph, List[List]] = [[] for _ in range(find_max_node_value(file))]
+def read_input_to_graph(file: str, reversed: bool = False) -> Tuple[Graph, List[List[Node]]]:
+    output_list: Union[Graph, List[None]] = [None for _ in range(find_max_node_value(file))]
+    to_nodes: List[List[Node]] = [[] for _ in range(find_max_node_value(file))]
     nodes: Dict[int, Node] = {}
     with open(file, 'r') as f:
         for line in f:
@@ -107,38 +108,60 @@ def read_input_to_graph(file: str, reversed: bool = False) -> Tuple[Graph, Graph
                 to_nodes[origin - 1].append(nodes[dest])
     return output_list, to_nodes
 
-
-def reverse_graph(g: Graph, finish_time_values: bool = False) -> Graph:
-    reverse_list: List[Union[Node, List]] = [Node(value=-(len(g)+1)) for _ in range(len(g))]
-    for node in g:
-        for to_node in node.to_nodes:
-            if finish_time_values:
-                reverse_list[to_node.value - 1].value = to_node.finish_time * (-1)
-            else:
-                reverse_list[to_node.value - 1].value = to_node.value
-
-            reverse_list[to_node.value - 1].to_nodes.append(node)
-            reverse_list[to_node.value - 1].seen = to_node.seen
-            reverse_list[to_node.value - 1].seen2 = to_node.seen2
-            reverse_list[to_node.value - 1].finish_time = to_node.finish_time
-            reverse_list[to_node.value - 1].leader = to_node.leader
-    return reverse_list
-
-
 #temp = read_input_to_graph('./ex1_test_cases/test1')
 temp_rev, to_nodes_rev = read_input_to_graph('./ex1_test_cases/test1', reversed=True)
+#temp, to_nodes = reverse_graph(temp_rev, to_nodes_rev)
 
 # TODO: the nodes that are in .to_nodes are not changed in this way and that is a problem!
 #       maybe I need to switch from using the priority queue.
+old_new_values: Dict[int, int] = {}  # old_value: new_value
+
+def reverse_graph(g: Graph, to_nodes: List[List[Node]]) -> Tuple[Graph, List[List[Node]]]:
+    '''
+    Returns the reversed to_nodes list for the same Graph g
+    '''
+    reverse_list: List[List[Node]] = [[] for _ in range(len(g))]
+    g_new: Union[Graph, List[None]] = [None for _ in range(len(g))]
+    for node in g:
+        for to_node in to_nodes[node.value - 1]:
+            reverse_list[to_node.value - 1].append(node)
+            if g_new[to_node.value - 1] is None:
+                g_new[to_node.value - 1] = to_node
+    return g_new, reverse_list
+
 def max_heap_value(node: Node) -> Node:
-    node.value = node.finish_time
-    node.finish_time = node.finish_time * (-1)
-    return node
+    node_copy = copy.deepcopy(node)
+    node_copy.old_value = node_copy.value
+    node_copy.value = node_copy.finish_time
+    node_copy.finish_time = node_copy.finish_time * (-1)
+    if node_copy.old_value not in old_new_values:
+        old_new_values[node_copy.old_value] = node_copy.value
+    return node_copy
 
 global pq
 pq = PriorityQueue()
 
-def dfs_loop(g: Graph,
+def switch_to_nodes_values(to_nodes: List[List[Node]]) -> List[List[Node]]:
+    '''
+    Switches the old values to the new finish_time values.
+    Also switches the relevant indices in the adjacency list
+    '''
+    nodes_list: List[List[Node]] = [None for _ in range(len(to_nodes))]
+    for i, item in enumerate(to_nodes):
+        for to_node in item:
+            if (to_node.value in old_new_values) and (not to_node.been_switched):
+                # TODO: consider assigning entire node and not just its value
+                #nodes_list[old_new_values[to_node.value].value - 1].append(to_node)
+                to_node.value = old_new_values[to_node.value]
+                to_node.been_switched = True
+        # find the new value for i+1
+        if (i+1) in old_new_values:
+            new_i = old_new_values[(i+1)]
+            nodes_list[new_i - 1] = item
+    return nodes_list
+
+
+def dfs_loop(g: Graph, to_nodes: List[List[Node]],
              finish_time_values: bool = False) -> None:
     global t
     global s
@@ -150,14 +173,14 @@ def dfs_loop(g: Graph,
             i = pq.popq()
             if not g[i.value - 1].seen2:
                 s = i
-                dfs(g, g[i.value - 1], second_pass=True)
+                dfs(g, g[i.value - 1], to_nodes, second_pass=True)
     else:
         for i in range(n-1, -1, -1):
             if not g[i].seen:
-                s = i
-                dfs(g, g[i])
+                s = i + 1
+                dfs(g, g[i], to_nodes)
 
-def dfs(g: Graph, i: Node, second_pass: bool = False) -> None:
+def dfs(g: Graph, i: Node, to_nodes: List[List[Node]], second_pass: bool = False) -> None:
     '''
     Depth-first-search
     '''
@@ -166,15 +189,15 @@ def dfs(g: Graph, i: Node, second_pass: bool = False) -> None:
     if second_pass:
         i.seen2 = True
         i.leader = g[s.value - 1]
-        for to_node in i.to_nodes:
+        for to_node in to_nodes[i.value - 1]:
             if not to_node.seen2:
-                dfs(g, to_node, second_pass=True)
+                dfs(g, to_node, to_nodes, second_pass=True)
     else:
         i.seen = True
-        #i.leader = g[s]
-        for to_node in i.to_nodes:
+        i.leader = g[s - 1]
+        for to_node in to_nodes[i.value - 1]:
             if not to_node.seen:
-                dfs(g, to_node)
+                dfs(g, to_node, to_nodes)
         t += 1
         i.finish_time = t
         pq.push(max_heap_value(i))
@@ -187,6 +210,9 @@ def get_scc_sizes(g: Graph) -> List[Tuple[int, int]]:
     return Counter(leaders).most_common(5)
 
 # trying
-dfs_loop(temp_rev)
-temp = reverse_graph(temp_rev)
-dfs_loop(temp, finish_time_values=True)
+dfs_loop(temp_rev, to_nodes_rev)
+temp, to_nodes = reverse_graph(temp_rev, to_nodes_rev)
+to_nodes = switch_to_nodes_values(to_nodes)
+dfs_loop(temp, to_nodes, finish_time_values=True)
+
+get_scc_sizes(temp)
