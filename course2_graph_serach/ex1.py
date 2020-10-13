04629d55e09a6ef650850ddf2603f72b16a54f09
Oren Epshtain <oren@gojuno.com>
2020-10-13 15:34:02 +0300

    managed to get working version for graph with missing nodes in destination or origin (but not both)

--- course2_graph_serach/ex1.py
+++ course2_graph_serach/ex1.py
@@ -12,7 +12,7 @@ from __future__ import annotations
 
 from typing import List, Optional, Dict, Tuple, TypeVar, Generic, Union
 
-from collections import Counter
+from collections import Counter, defaultdict
 
 import heapq
 
@@ -69,47 +69,79 @@ class PriorityQueue(Generic[T], list):
 Graph = List[Node]
 
 
-def find_max_node_value(file: str) -> int:
+def find_max_node_value(file: str) -> Tuple[int, int]:
+    '''
+    Returns max value for the current direction
+    and the total max value (either direction)
+    '''
     max_val: int = 0
     with open(file, 'r') as f:
-        for line in f:
+        for i, line in enumerate(f, start=1):
             row: List[int] = [int(item) for item in line.strip().split(' ')]
             if row[0] > max_val:
                 max_val = row[0]
             elif row[1] > max_val:
                 max_val = row[1]
-    return max_val
+    offset: int = i - max_val
+    return max_val, max_val + offset
 
 # TODO: if a value does not appear at all e.g. 1 to 10 except that 4
 #   does not appear, it will be None. This None can interfere, check that out!
 
 
-def read_input_to_graph(file: str, reversed: bool = False) -> Tuple[Graph, List[List[Node]]]:
-    output_list: Union[Graph, List[None]] = [None for _ in range(find_max_node_value(file))]
-    to_nodes: List[List[Node]] = [[] for _ in range(find_max_node_value(file))]
-    nodes: Dict[int, Node] = {}
+from_to_nodes: Dict[Node, List[Node]] = defaultdict(list)
+from_to_values: Dict[int, List[int]] = defaultdict(list)
+nodes: Dict[int, Node] = {}
+
+def read_input_to_graph(file: str, reversed: bool = False) -> Tuple[Graph, List[List[Node]], Graph]:
+    to_nodes_length, output_length = find_max_node_value(file)
+    offset = output_length - to_nodes_length
+    output_list: Union[Graph, List[None]] = [None for _ in range(to_nodes_length)]
+    output_general: Union[Graph, List[None]] = [None for _ in range(output_length - offset)]
+    to_nodes: List[List[Node]] = [[] for _ in range(to_nodes_length)]
+
     with open(file, 'r') as f:
-        for line in f:
+        for i, line in enumerate(f, start=1):
             row: List[int] = [int(item) for item in line.strip().split(' ')]
             origin, dest = row[0], row[1]
             # build nodes dict
             if origin not in nodes:
                 nodes[origin] = Node(value=origin)
+                from_to_values[origin].append(dest)
             if dest not in nodes:
                 nodes[dest] = Node(value=dest)
+                from_to_values[dest].append(origin)
+
+            if nodes[origin] not in from_to_nodes:
+                from_to_nodes[nodes[origin]].append(nodes[dest])
+            if nodes[dest] not in from_to_nodes:
+                from_to_nodes[nodes[dest]].append(nodes[origin])
+
+            if i not in nodes:  # for omitted cases
+                nodes[i] = Node(value=i)
 
             if reversed:
                 if output_list[dest - 1] is None:
                     output_list[dest - 1] = nodes[dest]
+                try:
+                    if output_general[i - 1] is None:
+                        output_general[i - 1] = nodes[i]
+                except IndexError:
+                    pass
                 to_nodes[dest - 1].append(nodes[origin])
             else:
                 if output_list[origin - 1] is None:
                     output_list[origin - 1] = nodes[origin]
+                try:
+                    if output_general[i - 1] is None:
+                        output_general[i - 1] = nodes[i]
+                except IndexError:
+                    pass
                 to_nodes[origin - 1].append(nodes[dest])
-    return output_list, to_nodes
+    return output_list, to_nodes, output_general
 
 #temp = read_input_to_graph('./ex1_test_cases/test1')
-temp_rev, to_nodes_rev = read_input_to_graph('./ex1_test_cases/test1', reversed=True)
+temp_rev, to_nodes_rev, g_graph = read_input_to_graph('./ex1_test_cases/test3', reversed=True)
 #temp, to_nodes = reverse_graph(temp_rev, to_nodes_rev)
 
 # TODO: the nodes that are in .to_nodes are not changed in this way and that is a problem!
@@ -123,10 +155,11 @@ def reverse_graph(g: Graph, to_nodes: List[List[Node]]) -> Tuple[Graph, List[Lis
     reverse_list: List[List[Node]] = [[] for _ in range(len(g))]
     g_new: Union[Graph, List[None]] = [None for _ in range(len(g))]
     for node in g:
-        for to_node in to_nodes[node.value - 1]:
-            reverse_list[to_node.value - 1].append(node)
-            if g_new[to_node.value - 1] is None:
-                g_new[to_node.value - 1] = to_node
+        if node is not None:
+            for to_node in to_nodes[node.value - 1]:
+                reverse_list[to_node.value - 1].append(node)
+                if g_new[to_node.value - 1] is None:
+                    g_new[to_node.value - 1] = to_node
     return g_new, reverse_list
 
 def max_heap_value(node: Node) -> Node:
@@ -141,23 +174,45 @@ def max_heap_value(node: Node) -> Node:
 global pq
 pq = PriorityQueue()
 
-def switch_to_nodes_values(to_nodes: List[List[Node]]) -> List[List[Node]]:
+def switch_to_nodes_values(to_nodes: List[List[Node]],
+                           to_nodes_rev: List[List[Node]]) -> List[List[Node]]:
     '''
     Switches the old values to the new finish_time values.
     Also switches the relevant indices in the adjacency list
     '''
-    nodes_list: List[List[Node]] = [None for _ in range(len(to_nodes))]
+    missings: List[Tuple[int, List[int]]] = []
+    nodes_list: List[List[Node]] = [[] for _ in range(len(to_nodes))]
+    for i, item in enumerate(to_nodes_rev):
+        if not item:
+            missings.append((i+1, from_to_values[i+1]))
+
     for i, item in enumerate(to_nodes):
-        for to_node in item:
-            if (to_node.value in old_new_values) and (not to_node.been_switched):
-                # TODO: consider assigning entire node and not just its value
-                #nodes_list[old_new_values[to_node.value].value - 1].append(to_node)
-                to_node.value = old_new_values[to_node.value]
-                to_node.been_switched = True
-        # find the new value for i+1
-        if (i+1) in old_new_values:
-            new_i = old_new_values[(i+1)]
-            nodes_list[new_i - 1] = item
+        if not item:    # if no outgoing connections
+            val_to_look = None
+            for tup in missings:
+                try:
+                    val_in_list = None
+                    val_in_list = tup[1].index(i+1)
+                    if val_in_list is not None:
+                        val_to_look = tup[0]
+                        break
+                except ValueError:
+                    continue
+            if (val_to_look is not None) and (val_to_look in old_new_values) and (not nodes[val_to_look].been_switched):
+                nodes[val_to_look].value = old_new_values[val_to_look]
+                nodes[val_to_look].been_switched = True
+                nodes_list[old_new_values[val_to_look] - 1].append(nodes[val_to_look])
+        else:
+            for to_node in item:
+                if (to_node.value in old_new_values) and (not to_node.been_switched):
+                    # TODO: consider assigning entire node and not just its value
+                    #nodes_list[old_new_values[to_node.value].value - 1].append(to_node)
+                    to_node.value = old_new_values[to_node.value]
+                    to_node.been_switched = True
+            # find the new value for i+1
+            if (i+1) in old_new_values:
+                new_i = old_new_values[(i+1)]
+                nodes_list[new_i - 1] = item
     return nodes_list
 
 
@@ -177,9 +232,10 @@ def dfs_loop(g: Graph, to_nodes: List[List[Node]],
                 dfs(g, g[index_i], to_nodes, second_pass=True)
     else:
         for i in range(n-1, -1, -1):
-            if not g[i].seen:
-                #s = i + 1
-                dfs(g, g[i], to_nodes)
+            if g[i] is not None:
+                if not g[i].seen:
+                    #s = i + 1
+                    dfs(g, g[i], to_nodes)
 
 def dfs(g: Graph, i: Node, to_nodes: List[List[Node]], second_pass: bool = False) -> None:
     '''
@@ -211,9 +267,9 @@ def get_scc_sizes(g: Graph) -> List[Tuple[int, int]]:
     return Counter(leaders).most_common(5)
 
 # trying
-dfs_loop(temp_rev, to_nodes_rev)
+dfs_loop(g_graph, to_nodes_rev)
 temp, to_nodes = reverse_graph(temp_rev, to_nodes_rev)
-to_nodes = switch_to_nodes_values(to_nodes)
-dfs_loop(temp, to_nodes, finish_time_values=True)
+to_nodes = switch_to_nodes_values(to_nodes, to_nodes_rev)
+dfs_loop(g_graph, to_nodes, finish_time_values=True)
 
-get_scc_sizes(temp)
+print(get_scc_sizes(temp))
