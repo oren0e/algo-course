4d64af36b552033de17dc2723f18ea84b9691cfd
Oren Epshtain <oren@gojuno.com>
2020-10-17 19:02:06 +0300

    function for reading the data ready.

--- course2_graph_serach/ex2.py
+++ course2_graph_serach/ex2.py
@@ -7,7 +7,7 @@ Implementing the Heap version of Dijkstra's algorithm
 '''
 import heapq
 
-from typing import List, TypeVar, Generic, Optional
+from typing import List, TypeVar, Generic, Optional, Tuple
 
 T = TypeVar('T')
 
@@ -41,4 +41,41 @@ class Heap(Generic[T]):
 
     def __repr__(self) -> str:
         return repr(self._data)
+
+# import data into adjacency list
+def find_max_node_value(file: str) -> int:
+    '''
+    Returns max value for the current direction
+    and the total max value (either direction)
+    '''
+    max_val: int = 0
+    with open(file, 'r') as f:
+        for i, line in enumerate(f, start=1):
+            row: List[int] = [int(item[0]) for item in line.strip().split(' ')]
+            if row[0] > max_val:
+                max_val = row[0]
+            #elif row[1] > max_val:
+             #   max_val = row[1]
+    offset: int = i - max_val
+    return max_val #, max_val + offset
+
+
+WeightedGraph = List[List[Tuple[int]]]
+
+
+def read_data(file: str) -> WeightedGraph:
+    data: WeightedGraph = [[] for _ in range(find_max_node_value(file))]
+
+    with open(file, 'r') as f:
+        for line in f:
+            cell_num = None
+            for i, col in enumerate(line.strip().split(' ')):
+                if i == 0:
+                    cell_num = int(col)
+                elif i > 0:
+                    data[cell_num - 1].append(tuple(int(t) for t in col.split(',')))
+    return data
+
+
+g: WeightedGraph = read_data('./ex2_test_cases/test1')
 
