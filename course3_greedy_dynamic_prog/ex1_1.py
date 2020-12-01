27a6c149b7b035dfc3cb6310d0e0c503cfc046d8
Oren Epshtain <oren@gojuno.com>
2020-12-01 20:06:52 +0200

    remembered that lengths should be accumulative.

--- course3_greedy_dynamic_prog/ex1_1.py
+++ course3_greedy_dynamic_prog/ex1_1.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List, Dict
+from typing import NamedTuple, List, Dict, Tuple
 
 from heapq import heappush, heappop
 
@@ -12,15 +12,20 @@ class Pair(NamedTuple):
 class PairDiff(NamedTuple):
     value: int
     index: int
+    weight: int
+    length: int
 
     def __lt__(self, other) -> bool:
         return self.value < other.value
+
+    def __gt__(self, other) -> bool:
+        return not self.value < other.value
 
 
 data: List[Pair] = []
 num_jobs: int
 
-with open('./ex1_test_cases/ex1_1_test0', 'r') as f:
+with open('./ex1_test_cases/ex1_1_test1', 'r') as f:
     for i, line in enumerate(f):
         if i == 0:
             num_jobs = int(line.strip())
@@ -36,42 +41,55 @@ def pair_with_bigger_weight(pair1: Pair, pair2: Pair) -> Pair:
         return pair1
     return pair2
 
-def get_schedule_indices(inputs: List[Pair]) -> List[int]:
+def get_schedule_indices(inputs: List[Pair]) -> Tuple[List[int], List[PairDiff]]:
     h: List[PairDiff] = []
     for pair in inputs:
-        item = PairDiff((-1) * (pair.weight - pair.length), pair.index)
+        item = PairDiff(value=(-1) * (pair.weight - pair.length), index=pair.index,
+                        weight=pair.weight, length=pair.length)
         pairs_dict[pair.index] = pair
         heappush(h, item)
 
-    res: List[int] = []
+    current_length: int = 0
+    res: List[PairDiff] = []
+    indices: List[int] = []
     while h:
         poped_item: PairDiff = heappop(h)
-        # TODO: check here if h[0] and poped equal and if so take the one with the bigger weight
-        if h:
-            if poped_item.value == h[0].value:
-                bigger_weight: Pair = pair_with_bigger_weight(pairs_dict[poped_item.index], pairs_dict[h[0].index])
-                if bigger_weight.index == h[0].index:
-                    second_pop = heappop(h)
-                    res.append(second_pop.index)
-        res.append(poped_item.index)
-    return res
+        current_length += poped_item.length
+        new_item = PairDiff(poped_item.value, poped_item.index,
+                            poped_item.weight, length=current_length)
+        res.append(new_item)
+        indices.append(poped_item.index)
+    return indices, res
 
-
-ordered_indices: List[int] = get_schedule_indices(data)
+ordered_indices, new_pairs = get_schedule_indices(data)
+diffs: List[int] = [pairs_dict[idx].weight - pairs_dict[idx].length for idx in ordered_indices]
 total: int = 0
-for ind in ordered_indices:
-    total += pairs_dict[ind].weight * pairs_dict[ind].length
-# for i, ind in enumerate(ordered_indices):
-#     try:
-#         next_index: int = ordered_indices[i + 1]
-#         if pairs_dict[ind].weight - pairs_dict[ind].length == pairs_dict[next_index].weight - pairs_dict[next_index].length:
-#             chosen_pair: Pair = pair_with_bigger_weight(pairs_dict[ind], pairs_dict[next_index])
-#             total += chosen_pair.weight * chosen_pair.length
-#         else:
-#             total += pairs_dict[ind].weight * pairs_dict[ind].length
-#     except IndexError:
-#         continue
+
+assert len(diffs) == len(ordered_indices)
+
+flag = False
+
+for i, ind in enumerate(ordered_indices):
+    if flag:
+        flag = False
+        continue
+    try:
+        next_index: int = ordered_indices[i + 1]
+        if diffs[i] == diffs[i + 1]:
+            chosen_pair: Pair = pair_with_bigger_weight(pairs_dict[ind], pairs_dict[next_index])
+            total += chosen_pair.weight * chosen_pair.length
+            flag = True
+            if chosen_pair.index == pairs_dict[ind].index:
+                total += pairs_dict[next_index].weight * pairs_dict[next_index].length
+            else:
+                total += pairs_dict[ind].weight * pairs_dict[ind].length
+        else:
+            total += pairs_dict[ind].weight * pairs_dict[ind].length
+    except IndexError:
+        continue
 
 # for test0
-assert total == 1175612
+#assert total == 1175612
+# for test1
+assert total == 74649
 print(total)
