ff90c1499e160a4021080f3363b01111081aec34
oren0e <countx@gmail.com>
2020-12-06 13:06:40 +0200

    all 3 test cases pass for question 3.

--- course3_greedy_dynamic_prog/ex1_3.py
+++ course3_greedy_dynamic_prog/ex1_3.py
@@ -46,8 +46,10 @@ class Heap(Generic[T]):
     def __repr__(self) -> str:
         return repr(self._data)
 
+
 class Edge(NamedTuple):
     value: int
+    with_vertex: int
 
 
 class Vertex:
@@ -85,20 +87,19 @@ def build_graph(file: str) -> Graph:
     res_list: Graph = [[] for _ in range(get_num_verticies(file))]
     vertex_dict: Dict[int, Vertex] = {}
     for tup in read_data_gen(file):
-        edge = Edge(value=int(tup[2]))
         if int(tup[0]) not in vertex_dict:
             vertex_dict[int(tup[0])] = Vertex(value=int(tup[0]))
         if int(tup[1]) not in vertex_dict:
             vertex_dict[int(tup[1])] = Vertex(value=int(tup[1]))
 
-        vertex_dict[int(tup[0])].edges.append(edge)
-        vertex_dict[int(tup[1])].edges.append(edge)
+        vertex_dict[int(tup[0])].edges.append(Edge(value=int(tup[2]), with_vertex=vertex_dict[int(tup[1])].value))
+        vertex_dict[int(tup[1])].edges.append(Edge(value=int(tup[2]), with_vertex=vertex_dict[int(tup[0])].value))
         res_list[int(tup[0]) - 1].append(vertex_dict[int(tup[1])])      # undirected graph: each edge appears twice!
         res_list[int(tup[1]) - 1].append(vertex_dict[int(tup[0])])
     return res_list
 
 
-g: Graph = build_graph('ex1_test_cases/ex1_3_test2')
+g: Graph = build_graph('ex1_test_cases/ex1_3_test0')
 
 
 def prim_overall_cost(g: Graph) -> int:
@@ -109,38 +110,30 @@ def prim_overall_cost(g: Graph) -> int:
     random.seed(902)
     x: List[Vertex] = []
     V: Set[int] = set(v for v in range(1, len(g) + 1))      # all vertices values
-    # v_minus_x: List[Vertex] = []
-    #t: List[Vertex] = []
-
 
     # choose first vertex randomly
     first_vertex: Vertex = random.choice(g[random.choice(range(len(g)))])
     x.append(first_vertex)
     frontier.add(first_vertex.value)
-    v_minus_x = [v for sub_list in g for v in sub_list if v.value != first_vertex.value]
+    v_minus_x: Set[Vertex] = set([v for sub_list in g for v in sub_list if v.value != first_vertex.value])
 
-    def get_key_vertex(v: Vertex) -> Optional[Vertex]:
-        """
-        General function to compute the key of a vertex,
-        where v is compared to other "many" vertices in 'other_vertices'
-        """
-        try:
-            candidate: Vertex = min([vertex for vertex in g[v.value - 1] if vertex.value in frontier], key=lambda x: x.cost)
-        except ValueError:
-            return None
-        return candidate
+    def compute_key(v: Vertex) -> int:
+        min_cost = float("inf")
+        for vertex in g[v.value - 1]:
+            if vertex.value in frontier:
+                for edge in vertex.edges:
+                    if edge.with_vertex == v.value:
+                        min_cost = min(min_cost, edge.value)
+        return min_cost
 
     # initial computation of keys
     h: Heap[Vertex] = Heap()
     vertices_in_heap: Dict[int, int] = {}   # {vertex.value: position_in_heap}
 
     for vertex in v_minus_x:
-        cheapset_vertex = get_key_vertex(vertex)
-        if cheapset_vertex:
-            vertex.key = cheapset_vertex.cost
-            vertex.key_vertex = cheapset_vertex
-        else:
-            vertex.key = float("inf")
+        vertex.key = compute_key(vertex)
+        # if not vertex.key:
+        #     vertex.key = float("inf")
         if vertex.value not in pushed_to_heap:
             h.heap_push(vertex)
             pushed_to_heap.add(vertex.value)
@@ -155,17 +148,17 @@ def prim_overall_cost(g: Graph) -> int:
         update_mapping()
         x.append(popped_vertex)
         frontier.add(popped_vertex.value)
-        v_minus_x = [v for sub_list in g for v in sub_list if v.value != popped_vertex.value]
+        v_minus_x = {v for v in v_minus_x if v.value != popped_vertex.value}
+        #v_minus_x = [v for sub_list in g for v in sub_list if v.value != popped_vertex.value]
         # maintain invariant 2
-        popped_vertex_edges: List[Vertex] = g[popped_vertex.value - 1]
-        for member_vertex in popped_vertex_edges:
+        popped_vertex_vertices: List[Vertex] = g[popped_vertex.value - 1]
+        for member_vertex in popped_vertex_vertices:
             if member_vertex.value not in frontier:
                 #if not h.is_empty:
                 h.heap_delete(vertices_in_heap[member_vertex.value])
                 update_mapping()
                 # recompute key of w
-                member_vertex.key = min(member_vertex.key, member_vertex.cost)
-                member_vertex.key_vertex = popped_vertex
+                member_vertex.key = min(member_vertex.key, compute_key(member_vertex))
                 h.heap_push(member_vertex)
                 # update dictionary
                 update_mapping()
